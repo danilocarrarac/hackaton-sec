05a639e16198b059713facbba575fd26c437c3ac
dangelim <dangelim@uolinc.com>
2019-08-21 15:40:56 -0300

    Aplicacao do MD5

---
+++ src/test/java/sanitization/MD5Test.java
@@ -0,0 +1,14 @@
+package sanitization;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MD5Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
